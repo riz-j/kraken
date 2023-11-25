6f263e8610f8e7f951efacfe6bacbe73673950c1
riz-j <adiputra979@gmail.com>
2023-11-26 09:56:36 +1100

    API and Database query in API works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use axum::{extract::Path, routing::get, Error, Router};
 use mysqlx::{
     clients::{city_client, country_client},
     models::{
@@ -8,10 +9,30 @@ use mysqlx::{
 // use mysqlx::models::country_model::InsertCountry;
 use sqlx::sqlite::SqlitePoolOptions;
 
+async fn get_country_by_id(Path(user_id): Path<i64>) -> String {
+    let db_file = "./my_database.db";
+    let pool = SqlitePoolOptions::new()
+        .connect(&format!("sqlite:{}", db_file))
+        .await
+        .unwrap();
+
+    let country = country_client::select_country(&pool, user_id)
+        .await
+        .unwrap();
+
+    format!("Country: {:?}", country)
+}
+
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-    let db_file = "./my_database.db";
+    let app = Router::new().route("/country/:country_id", get(get_country_by_id));
 
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+    let db_file = "./my_database.db";
     let pool = SqlitePoolOptions::new()
         .connect(&format!("sqlite:{}", db_file))
         .await?;
