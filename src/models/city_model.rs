f3fe7b88d1db531fffd4af3604e6ad4af39109b4
riz-j <adiputra979@gmail.com>
2023-11-26 09:09:12 +1100

    Extend city model

--- src/models/city_model.rs
+++ src/models/city_model.rs
@@ -1,10 +1,24 @@
+use crate::clients::country_client;
+use sqlx::{Error, Pool, Sqlite};
+
+use super::country_model::SelectCountry;
+
+#[derive(Debug, sqlx::FromRow)]
 #[allow(dead_code)]
 pub struct SelectCity {
     pub id: i64,
     pub name: String,
+    pub country_id: Option<i64>,
 }
 
 #[allow(dead_code)]
 pub struct InsertCity {
     pub name: String,
+    pub country_id: Option<i64>,
+}
+
+impl SelectCity {
+    pub async fn get_country(&self, pool: &Pool<Sqlite>) -> Result<SelectCountry, Error> {
+        country_client::select_country(pool, self.country_id.unwrap()).await
+    }
 }
