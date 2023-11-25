f5092394077b617abcee92559952b6f3fff2c626
riz-j <adiputra979@gmail.com>
2023-11-26 09:08:48 +1100

    Add select city

--- src/clients/city_client.rs
+++ src/clients/city_client.rs
@@ -1,4 +1,4 @@
-use crate::models::city_model::InsertCity;
+use crate::models::city_model::{InsertCity, SelectCity};
 use sqlx::{Error, Pool, Sqlite};
 
 pub async fn insert_city(pool: &Pool<Sqlite>, insert_city: &InsertCity) -> Result<(), Error> {
@@ -16,4 +16,18 @@ pub async fn insert_city(pool: &Pool<Sqlite>, insert_city: &InsertCity) -> Resul
     println!("City '{}' has been inserted!", insert_city.name);
 
     Ok(())
+}
+
+pub async fn select_city(pool: &Pool<Sqlite>, city_id: i64) -> Result<SelectCity, Error> {
+    let city = sqlx::query_as::<_, SelectCity>(
+        "
+        SELECT id, name, country_id
+        FROM cities WHERE id = $1;
+        ",
+    )
+    .bind(city_id)
+    .fetch_one(pool)
+    .await?;
+
+    Ok(city)
 }
