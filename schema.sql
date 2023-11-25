e33e6fd29132cb0df62f41995054d1c2be64d1a2
riz-j <adiputra979@gmail.com>
2023-11-26 08:45:35 +1100

    Modify schema to add country_id foreign key

--- schema.sql
+++ schema.sql
@@ -1,10 +1,12 @@
 CREATE TABLE sqlite_sequence(name,seq);
-CREATE TABLE cities (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL
-        );
 CREATE TABLE countries (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     continent TEXT
+);
+CREATE TABLE IF NOT EXISTS "cities" (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    country_id INTEGER,
+    FOREIGN KEY (country_id) REFERENCES countries(id)
 );
