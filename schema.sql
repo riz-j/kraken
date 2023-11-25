64d33fbf0b0cb8864b2cbe9ff0b47d4db072a0e8
riz-j <adiputra979@gmail.com>
2023-11-25 23:58:53 +1100

    Code clean up

--- schema.sql
+++ schema.sql
@@ -8,13 +8,3 @@ CREATE TABLE countries (
     name TEXT NOT NULL,
     continent TEXT
 );
-CREATE TABLE sqlite_sequence(name,seq);
-CREATE TABLE cities (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL
-        );
-CREATE TABLE countries (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    continent TEXT
-);
