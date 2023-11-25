2d3af8d2cf536d4c4bb7bc7ff624ce33300764b3
riz-j <adiputra979@gmail.com>
2023-11-25 23:56:27 +1100

    Add schema.sql

---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE cities (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        );
+CREATE TABLE countries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    continent TEXT
+);
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE cities (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        );
+CREATE TABLE countries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    continent TEXT
+);
