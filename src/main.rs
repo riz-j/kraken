3f60b212fa2801f3a0174d61e6f3884d06e1ea93
riz-j <adiputra979@gmail.com>
2023-11-26 14:08:26 +1100

    Add static file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use axum::http::StatusCode;
-use axum::routing::post;
+use axum::routing::{get_service, post};
 use axum::{extract::Path, routing::get, Json, Router};
 use kraken::clients::{city_client, country_client};
 use kraken::models::country_model::{InsertCountry, SelectCountry};
+use tower_http::services::ServeDir;
 
 async fn get_country_by_id(Path(country_id): Path<i64>) -> String {
     let country = country_client::select_country(country_id).await.unwrap();
@@ -23,6 +24,10 @@ async fn get_city_by_id(Path(city_id): Path<i64>) -> String {
     format!("City: {:?}\nCountry: {:?}", city, country)
 }
 
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./public")))
+}
+
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     kraken::db::init_pool().await;
@@ -30,7 +35,8 @@ async fn main() -> Result<(), sqlx::Error> {
     let app = Router::new()
         .route("/country/:country_id", get(get_country_by_id))
         .route("/country", post(create_country))
-        .route("/city/:city_id", get(get_city_by_id));
+        .route("/city/:city_id", get(get_city_by_id))
+        .fallback_service(routes_static());
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
