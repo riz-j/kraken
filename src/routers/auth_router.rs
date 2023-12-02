d61daf772dbb6abe2399b84caadf2669a26b99c8
riz-j <adiputra979@gmail.com>
2023-12-02 20:58:07 +1100

    Add demonstration of how JWT can be generated

--- src/routers/auth_router.rs
+++ src/routers/auth_router.rs
@@ -5,6 +5,30 @@ use axum::{
     Router,
 };
 use cookie::Cookie;
+use jsonwebtoken::{encode, EncodingKey, Header};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    user_id: i64,
+    iat: usize,
+    exp: usize,
+}
+
+async fn jwt() -> impl IntoResponse {
+    let key = EncodingKey::from_secret("my_very_secretive_secret".as_ref());
+
+    let timestamp_now = chrono::Utc::now().timestamp() as usize;
+    let my_claims = Claims {
+        user_id: 123123,
+        iat: timestamp_now,
+        exp: timestamp_now + (7 * 24 * 60 * 60),
+    };
+
+    let token = encode(&Header::default(), &my_claims, &key);
+
+    token.unwrap()
+}
 
 async fn login() -> impl IntoResponse {
     let mut cookie = Cookie::new("KRAKEN_AUTH", "my_secret_token");
@@ -23,5 +47,5 @@ async fn login() -> impl IntoResponse {
 }
 
 pub fn auth_router() -> Router {
-    Router::new().route("/auth/login", post(login))
+    Router::new().route("/auth/login", post(login).get(jwt))
 }
