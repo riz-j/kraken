411cd0fc844bf7a8e89a8785f7cf1a697668ddd6
riz-j <adiputra979@gmail.com>
2023-12-06 08:40:43 +1100

    Update database sql dump and schema

--- dump.sql
+++ dump.sql
@@ -1,56 +1,70 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-
 CREATE TABLE IF NOT EXISTS "countries" (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     continent TEXT
-);
-INSERT INTO countries VALUES (1, 'Poland','Europe');
-INSERT INTO countries VALUES (2, 'United Kingdom','Europe');
-INSERT INTO countries VALUES (3, 'Indonesia','Asia');
-INSERT INTO countries VALUES (4, 'Thailand','Asia');
-INSERT INTO countries VALUES (5, 'Botswana','Africa');
-INSERT INTO countries VALUES (6, 'South Africa','Africa');
-INSERT INTO countries VALUES (7, 'France', 'Europe');
-INSERT INTO countries VALUES (8, 'Japan', 'Asia');
-INSERT INTO countries VALUES (9, 'Brazil', 'South America');
-INSERT INTO countries VALUES (10, 'Canada', 'North America');
-INSERT INTO countries VALUES (11, 'Australia', 'Australia');
-INSERT INTO countries VALUES (12, 'Germany', 'Europe');
-INSERT INTO countries VALUES (13, 'Kenya', 'Africa');
-INSERT INTO countries VALUES (14, 'Mexico', 'North America');
-INSERT INTO countries VALUES (15, 'India', 'Asia');
-INSERT INTO countries VALUES (16, 'New Zealand', 'Oceania');
-
+, is_archived BOOLEAN NOT NULL DEFAULT 0);
+INSERT INTO countries VALUES(1,'Poland','Europe',0);
+INSERT INTO countries VALUES(2,'United Kingdom','Europe',0);
+INSERT INTO countries VALUES(3,'Indonesia','Asia',0);
+INSERT INTO countries VALUES(4,'Thailand','Asia',0);
+INSERT INTO countries VALUES(5,'Botswana','Africa',0);
+INSERT INTO countries VALUES(6,'South Africa','Africa',0);
+INSERT INTO countries VALUES(7,'France','Europe',0);
+INSERT INTO countries VALUES(8,'Japan','Asia',0);
+INSERT INTO countries VALUES(9,'Brazil','South America',0);
+INSERT INTO countries VALUES(10,'Canada','North America',0);
+INSERT INTO countries VALUES(11,'Australia','Australia',0);
+INSERT INTO countries VALUES(12,'Germany','Europe',0);
+INSERT INTO countries VALUES(13,'Kenya','Africa',0);
+INSERT INTO countries VALUES(14,'Mexico','North America',0);
+INSERT INTO countries VALUES(15,'India','Asia',0);
+INSERT INTO countries VALUES(16,'New Zealand','Oceania',0);
+INSERT INTO countries VALUES(17,'Barbados','North America',0);
+INSERT INTO countries VALUES(18,'Senegal','Africa',0);
+INSERT INTO countries VALUES(19,'Mongolia','Asia',0);
+INSERT INTO countries VALUES(23,'Italy','Europe',0);
+INSERT INTO countries VALUES(24,'Guinea','Africa',0);
+INSERT INTO countries VALUES(28,'Malta','Europe',0);
 CREATE TABLE IF NOT EXISTS "cities" (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     country_id INTEGER,
     FOREIGN KEY (country_id) REFERENCES countries(id)
 );
-INSERT INTO cities (name, country_id) VALUES ('Warsaw', 1);
-INSERT INTO cities (name, country_id) VALUES ('Krakow', 1);
-INSERT INTO cities (name, country_id) VALUES ('London', 2);
-INSERT INTO cities (name, country_id) VALUES ('Manchester', 2);
-INSERT INTO cities (name, country_id) VALUES ('Jakarta', 3);
-INSERT INTO cities (name, country_id) VALUES ('Bali', 3);
-INSERT INTO cities (name, country_id) VALUES ('Bangkok', 4);
-INSERT INTO cities (name, country_id) VALUES ('Chiang Mai', 4);
-INSERT INTO cities (name, country_id) VALUES ('Johannesburg', 6);
-INSERT INTO cities (name, country_id) VALUES ('Cape Town', 6);
-INSERT INTO cities (name, country_id) VALUES ('Paris', 7);
-INSERT INTO cities (name, country_id) VALUES ('Lyon', 7);
-INSERT INTO cities (name, country_id) VALUES ('Tokyo', 8);
-INSERT INTO cities (name, country_id) VALUES ('Kyoto', 8);
-INSERT INTO cities (name, country_id) VALUES ('Rio de Janeiro', 9);
-INSERT INTO cities (name, country_id) VALUES ('Sao Paulo', 9);
-INSERT INTO cities (name, country_id) VALUES ('Toronto', 10);
-INSERT INTO cities (name, country_id) VALUES ('Vancouver', 10);
-INSERT INTO cities (name, country_id) VALUES ('Sydney', 11);
-INSERT INTO cities (name, country_id) VALUES ('Melbourne', 11);
-
-
+INSERT INTO cities VALUES(1,'Warsaw',1);
+INSERT INTO cities VALUES(2,'Krakow',1);
+INSERT INTO cities VALUES(3,'London',2);
+INSERT INTO cities VALUES(4,'Manchester',2);
+INSERT INTO cities VALUES(5,'Jakarta',3);
+INSERT INTO cities VALUES(6,'Bali',3);
+INSERT INTO cities VALUES(7,'Bangkok',4);
+INSERT INTO cities VALUES(8,'Chiang Mai',4);
+INSERT INTO cities VALUES(9,'Johannesburg',6);
+INSERT INTO cities VALUES(10,'Cape Town',6);
+INSERT INTO cities VALUES(11,'Paris',7);
+INSERT INTO cities VALUES(12,'Lyon',7);
+INSERT INTO cities VALUES(13,'Tokyo',8);
+INSERT INTO cities VALUES(14,'Kyoto',8);
+INSERT INTO cities VALUES(15,'Rio de Janeiro',9);
+INSERT INTO cities VALUES(16,'Sao Paulo',9);
+INSERT INTO cities VALUES(17,'Toronto',10);
+INSERT INTO cities VALUES(18,'Vancouver',10);
+INSERT INTO cities VALUES(19,'Sydney',11);
+INSERT INTO cities VALUES(20,'Melbourne',11);
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT
+);
+INSERT INTO users VALUES(1,'joe@gmail.com','12345','Joe','Schmoe');
+INSERT INTO users VALUES(2,'jeremy@gmail.com','12345','Jeremy','Sinister');
+INSERT INTO users VALUES(3,'jenny@gmail.com','12345','Jenny','Gump');
+INSERT INTO users VALUES(4,'holly@gmail.com','12345','Holly','Hills');
+INSERT INTO users VALUES(5,'sam@gmail.com','12345','Sam','Smith');
 DELETE FROM sqlite_sequence;
-INSERT INTO sqlite_sequence VALUES('countries',7);
+INSERT INTO sqlite_sequence VALUES('countries',28);
 COMMIT;
