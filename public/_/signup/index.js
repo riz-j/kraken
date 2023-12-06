0e6d1d2e613eaadedfe16051a75a1a9821532f10
riz-j <adiputra979@gmail.com>
2023-12-06 14:23:12 +1100

    Make indentation tabs in file smaller

--- public/_/signup/index.js
+++ public/_/signup/index.js
@@ -1,39 +1,37 @@
 const { createApp, ref } = Vue;
 
-createApp({
-    setup() {
-        const email = ref("");
-        const password = ref("");
-        const confirm_password = ref("");
-        const first_name = ref("");
-        const last_name = ref("");
+createApp({ setup() {
+    const email = ref("");
+    const password = ref("");
+    const confirm_password = ref("");
+    const first_name = ref("");
+    const last_name = ref("");
 
-        const handleSignUp = () => {
-            if (password.value !== confirm_password.value) {
-                console.log("Password does not match!");
-                return;
-            }
-            fetch("/auth/signup", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({
-                    email: email.value,
-                    password: password.value,
-                    first_name: first_name.value,
-                    last_name: last_name.value
-                })
-            }).then(res => {
-                if (res.ok) { window.location.href = "/countries" }
-            });
+    const handleSignUp = () => {
+        if (password.value !== confirm_password.value) {
+            console.log("Password does not match!");
+            return;
         }
-
-        return {
-            email,
-            password,
-            confirm_password,
-            first_name,
-            last_name,
-            handleSignUp
-        }
+        fetch("/auth/signup", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                email: email.value,
+                password: password.value,
+                first_name: first_name.value,
+                last_name: last_name.value
+            })
+        }).then(res => {
+            if (res.ok) { window.location.href = "/countries" }
+        });
     }
-}).mount("#app")
+
+    return {
+        email,
+        password,
+        confirm_password,
+        first_name,
+        last_name,
+        handleSignUp
+    }
+}}).mount("#app")
