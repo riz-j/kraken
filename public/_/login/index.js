f3b6161fa4ab1164f79f37f0d30459a287069e88
Rizki Judojono <rizki.judojono@aroflo.com>
2023-12-06 03:10:08 +0000

    Move js to individual index.js files AND make vue 3 available from local CDN

---
+++ public/_/login/index.js
@@ -0,0 +1,29 @@
+const { createApp, ref } = Vue;
+
+createApp({
+	setup() {
+		const email = ref("");
+		const password = ref("");
+
+		const handleLogin = () => {
+			fetch("/auth/login", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({
+					email: email.value,
+					password: password.value
+				})
+			}).then(res => {
+				if (res.ok) {
+					window.location.href = "/countries";
+				}
+			});
+		};
+
+		return {
+			email,
+			password,
+			handleLogin
+		}
+	}
+}).mount("#app")
