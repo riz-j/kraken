f0475ae5badc6bcff829248036d05c83524e0897
riz-j <adiputra979@gmail.com>
2023-12-17 14:54:06 +1100

    Fix issue with alias warning during build

--- frontend-main/vite.config.ts
+++ frontend-main/vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import Generouted from '@generouted/react-router/plugin'
@@ -5,4 +6,9 @@ import Generouted from '@generouted/react-router/plugin'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), Generouted()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }
+    ]
+  }
 })
