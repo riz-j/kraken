f5f29d8a683a898717e719cb989196b24b8ef2da
riz-j <adiputra979@gmail.com>
2024-01-27 13:17:49 +1100

    Progress on rpc router

--- src/routers/rpc_router.rs
+++ src/routers/rpc_router.rs
@@ -1,19 +1,29 @@
 use axum::{response::IntoResponse, routing::post, Json, Router};
 
+fn print_number(number: String) {
+    println!("Number is {}", number);
+}
 fn print_name(name: String) {
     println!("Name is {}", name);
 }
 
 #[derive(serde::Deserialize, serde::Serialize)]
 struct RpcRequest {
+    id: String,
     method: String,
     param: String,
 }
 
 async fn handle_rpc_router(Json(payload): Json<RpcRequest>) -> impl IntoResponse {
-    if payload.method == "print_name" {
-        print_name(payload.param);
+    let rpc_req_id = payload.id;
+
+    match payload.method.as_str() {
+        "print_name" => print_name(payload.param),
+        "print_number" => print_number(payload.param),
+        _ => unimplemented!(),
     }
+
+    rpc_req_id
 }
 
 pub fn rpc_router() -> Router {
