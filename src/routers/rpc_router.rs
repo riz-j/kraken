d39bf665d483b13899c80b81b9ea6566aaf126ca
riz-j <adiputra979@gmail.com>
2024-01-27 13:39:37 +1100

    Progress on rpc router

--- src/routers/rpc_router.rs
+++ src/routers/rpc_router.rs
@@ -1,25 +1,51 @@
 use axum::{response::IntoResponse, routing::post, Json, Router};
+use serde::{Deserialize, Serialize};
 
-fn print_number(number: String) {
-    println!("Number is {}", number);
-}
-fn print_name(name: String) {
-    println!("Name is {}", name);
+#[derive(Deserialize, Serialize)]
+struct PrintNameParams {
+    name: String,
 }
 
-#[derive(serde::Deserialize, serde::Serialize)]
+#[derive(Deserialize, Serialize)]
+struct PrintNumberParams {
+    number: i32,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(untagged)]
+enum RpcParams {
+    PrintName(PrintNameParams),
+    PrintNumber(PrintNumberParams),
+}
+
+#[derive(Deserialize, Serialize)]
 struct RpcRequest {
     id: String,
     method: String,
-    param: String,
+    #[serde(rename = "params")]
+    param: RpcParams,
+}
+
+fn print_number(number: i32) {
+    println!("Number is {}", number);
+}
+
+fn print_name(name: String) {
+    println!("Name is {}", name);
 }
 
 async fn handle_rpc_router(Json(payload): Json<RpcRequest>) -> impl IntoResponse {
     let rpc_req_id = payload.id;
 
     match payload.method.as_str() {
-        "print_name" => print_name(payload.param),
-        "print_number" => print_number(payload.param),
+        "print_name" => match payload.param {
+            RpcParams::PrintName(params) => print_name(params.name),
+            _ => eprintln!("Invalid params for method {}", payload.method),
+        },
+        "print_number" => match payload.param {
+            RpcParams::PrintNumber(params) => print_number(params.number),
+            _ => eprintln!("Invalid params for method {}", payload.method),
+        },
         _ => unimplemented!(),
     }
 
