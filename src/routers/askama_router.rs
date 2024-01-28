3beb7dbe7f2b323b963076b5ad440785a025f6f5
riz-j <adiputra979@gmail.com>
2024-01-28 16:10:48 +1100

    Add templating for country

--- src/routers/askama_router.rs
+++ src/routers/askama_router.rs
@@ -1,6 +1,9 @@
 use crate::{
-    schemas::{city_schema::CitySummarizedSchema, country_schema::CountrySummarizedSchema},
-    stores::city_store,
+    schemas::{
+        city_schema::CitySummarizedSchema,
+        country_schema::{CountryExtendedSchema, CountrySummarizedSchema},
+    },
+    stores::{city_store, country_store},
 };
 use askama::Template;
 use axum::{extract::Path, response::Html, routing::get, Router};
@@ -29,6 +32,27 @@ async fn render_city_page(Path(city_id): Path<i64>) -> Html<String> {
     Html(template_string)
 }
 
+#[derive(Template)]
+#[template(path = "country.template.html")]
+#[allow(dead_code)]
+struct CountryTemplate {
+    country: CountryExtendedSchema,
+}
+
+async fn render_country_page(Path(country_id): Path<i64>) -> Html<String> {
+    let country = country_store::select_country(country_id).await.unwrap();
+    let country_extended = country.into_extended_schema().await;
+
+    let country_template = CountryTemplate {
+        country: country_extended,
+    };
+
+    let template_string = country_template.render().unwrap();
+    Html(template_string)
+}
+
 pub fn askama_router() -> Router {
-    Router::new().route("/pages/cities/:city_id", get(render_city_page))
+    Router::new()
+        .route("/pages/cities/:city_id", get(render_city_page))
+        .route("/pages/countries/:country_id", get(render_country_page))
 }
