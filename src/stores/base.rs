ccb67815df7a38cc457dc522d3bb4dde10b70006
riz-j <112792090+riz-j@users.noreply.github.com>
2024-02-01 22:13:23 +1100

    Update base.rs

--- src/stores/base.rs
+++ src/stores/base.rs
@@ -2,7 +2,9 @@ use crate::ctx::Ctx;
 use async_trait::async_trait;
 
 #[async_trait]
-pub trait BaseStore<T> {
-    async fn get(&self, ctx: &Ctx, id: i64) -> Result<T, sqlx::Error>;
-    async fn list(&self, ctx: &Ctx) -> Result<Vec<T>, sqlx::Error>;
+pub trait BaseStore<S, I, U> {
+    async fn select(&self, ctx: &Ctx, id: i64) -> Result<S, sqlx::Error>;
+    async fn list(&self, ctx: &Ctx) -> Result<Vec<S>, sqlx::Error>;
+    async fn insert(&self, ctx: &Ctx, item: I) -> Result<i64, sqlx::Error>;
+    async fn update(&self, ctx: &Ctx, item: U) -> Resilt<i64, sqlx::Error>;
 }
